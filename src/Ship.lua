85fbeae1643f2daeb606be0182b5c4b92b395b57
phansonloc1999 <phansonloc1999@gmail.com>
2019-01-19 09:38:04 +0700

    Add Ship

---
+++ src/Ship.lua
@@ -0,0 +1,15 @@
+---@class Ship
+Ship = Class{}
+
+---@param shipID number
+function Ship:init(x, y, shipID, health)
+    self._x, self._y = x, y
+    self._shipID = shipID
+    self._health = health
+end
+
+function Ship:render()
+    love.graphics.draw(gTextures.ships, gFrames.ships[self._shipID], self._x, self._y)
+end
+
+function Ship:update(dt) end
