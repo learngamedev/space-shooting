d02afd765ab662dfeb2c68d216211e100bbe8a4a
phansonloc1999 <phansonloc1999@gmail.com>
2019-01-19 21:11:18 +0700

    Add width, height, movement restriction

--- src/Player.lua
+++ src/Player.lua
@@ -7,6 +7,7 @@ function Player:init(x, y)
     self._ship = Ship(x, y, 3, 100)
     self._bulletID = 1
     self._bullets = {} ---@type Bullet[]
+    self._width, self._height = gFrames.ships[3].width, gFrames.ships[3].height
 end
 
 function Player:render()
@@ -35,15 +36,15 @@ end
 
 function Player:move(dt)
     if (love.keyboard.isDown("left")) then
-        self._ship._x = self._ship._x - PLAYER_SPEED * dt
+        self._ship._x = math.max(0, self._ship._x - PLAYER_SPEED * dt)
     elseif (love.keyboard.isDown("right")) then
-        self._ship._x = self._ship._x + PLAYER_SPEED * dt
+        self._ship._x = math.min(WINDOW_WIDTH - self._width, self._ship._x + PLAYER_SPEED * dt)
     end
 
     if (love.keyboard.isDown("up")) then
-        self._ship._y = self._ship._y - PLAYER_SPEED * dt
+        self._ship._y = math.max(0, self._ship._y - PLAYER_SPEED * dt)
     elseif (love.keyboard.isDown("down")) then
-        self._ship._y = self._ship._y + PLAYER_SPEED * dt
+        self._ship._y = math.min(WINDOW_HEIGHT - self._height, self._ship._y + PLAYER_SPEED * dt)
     end
 end
 
@@ -52,7 +53,7 @@ function Player:shoot()
         table.insert(
             self._bullets,
             Bullet(
-                self._ship._x + gFrames.ships[3].width / 2 - gFrames.bullets[self._bulletID].width / 2,
+                self._ship._x + self._width / 2 - gFrames.bullets[self._bulletID].width / 2,
                 self._ship._y - 10,
                 self._bulletID
             )
