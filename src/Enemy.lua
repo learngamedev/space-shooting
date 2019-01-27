de832803c639dce152cac48c1476ff7f00aaefcc
phansonloc1999 <phansonloc1999@gmail.com>
2019-01-27 10:38:23 +0700

    Add Enemy

---
+++ src/Enemy.lua
@@ -0,0 +1,87 @@
+---@class Enemy
+Enemy = Class {}
+
+function Enemy:init(x, y, shipID, bulletID, hp)
+    self._ship = Ship(x, y, shipID, hp)
+
+    self._dx, self._dy = 0, 0
+    self._speed = 120
+    self._chasingPlayer = false
+
+    self._bulletID = bulletID
+    self._bullets = {} ---@type Bullet[]
+    self._cooldownTimer = 0
+end
+
+function Enemy:render()
+    self._ship:render()
+
+    for i = 1, #self._bullets do
+        if (self._bullets[i]) then
+            self._bullets[i]:render()
+        end
+    end
+end
+
+function Enemy:update(dt)
+    self._ship._x = math.floor(self._ship._x + self._speed * self._dx * dt)
+    self._ship._y = math.floor(self._ship._y + self._speed * self._dy * dt)
+
+    self:shoot(dt)
+end
+
+function Enemy:setVelocity(dx, dy)
+    self._dx, self._dy = dx or 0, dy or 0
+end
+
+---@param target Bullet
+function Enemy:hit(target)
+    if
+        (checkCollision(
+            self._ship._x,
+            self._ship._y,
+            gFrames.ships[self._ship._shipID].width,
+            gFrames.ships[self._ship._shipID].height,
+            target._x,
+            target._y,
+            gFrames.bullets[target._bulletID].width,
+            gFrames.bullets[target._bulletID].height
+        ))
+     then
+        return true
+    end
+    return false
+end
+
+function Enemy:chasePlayer(player)
+    local dx, dy = 0, 0
+    if (self._ship._x > player._ship._x) then
+        dx = -1
+    elseif (self._ship._x < player._ship._x) then
+        dx = 1
+    end
+    if (self._ship._y > player._ship._y) then
+        dy = -1
+    elseif (self._ship._y < player._ship._y) then
+        dy = 1
+    end
+    self:setVelocity(dx, dy)
+end
+
+function Enemy:shoot(dt)
+    if (self._cooldownTimer > 0) then
+        self._cooldownTimer = math.max(0, self._cooldownTimer - 5 * dt)
+    else
+        table.insert(
+            self._bullets,
+            Bullet(
+                self._ship._x + gFrames.ships[self._ship._shipID].width / 2 + gFrames.bullets[self._bulletID].width / 2,
+                self._ship._y + gFrames.ships[self._ship._shipID].height + 3,
+                self._bulletID,
+                3.14
+            )
+        )
+        self._bullets[#self._bullets]._dy = 1
+        self._cooldownTimer = BULLETS[self._bulletID].cooldown
+    end
+end
--- src/States/PlayState.lua
+++ src/States/PlayState.lua
@@ -4,8 +4,10 @@ PlayState = Class {__includes = BaseState}
 function PlayState:init()
     self._player = Player(WINDOW_WIDTH / 2 - 23, 200)
     self._backgroundsY = {0, nil, -480, -960}
-    self._backgroundScrollSpeed = 130
+    self._backgroundScrollSpeed = 200
     self._crates = {} ---@type Crate[]
+
+    self._enemies = {} ---@type Enemy[]
 end
 
 function PlayState:render()
@@ -27,11 +29,15 @@ function PlayState:render()
     end
 
     self._player:render()
+
+    for i = 1, #self._enemies do
+        self._enemies[i]:render()
+    end
 end
 
 function PlayState:update(dt)
     for k, i in ipairs({1, 3, 4}) do
-        self._backgroundsY[i] = self._backgroundsY[i] + self._backgroundScrollSpeed * dt
+        self._backgroundsY[i] = math.floor(self._backgroundsY[i] + self._backgroundScrollSpeed * dt)
         if (self._backgroundsY[i] >= WINDOW_HEIGHT) then
             self._backgroundsY[i] = -959
         end
@@ -51,13 +57,47 @@ function PlayState:update(dt)
                             table.remove(self._player._bullets, j)
                             self._crates[i]:pop()
                             break
-                        end 
+                        end
                     end
                 end
             else
                 if (self._crates[i]:collides(self._player._ship)) then
                     self._player:getPowerup(self._crates[i])
                     table.remove(self._crates, i)
+                end
+            end
+        end
+    end
+
+    for i = 1, #self._enemies do
+        if (self._enemies[i]) then
+            if (self._enemies[i]._chasingPlayer) then
+                self._enemies[i]:chasePlayer(self._player)
+            end
+
+            for k = 1, #self._enemies[i]._bullets do
+                if (self._enemies[i]._bullets[k]) then
+                    self._enemies[i]._bullets[k]:update(dt)
+        
+                    if (self._enemies[i]._bullets[k]._y > WINDOW_HEIGHT) then
+                        table.remove(self._enemies[i]._bullets, i)
+                    end
+                end
+            end
+
+            self._enemies[i]:update(dt)
+    
+            for j = 1, #self._player._bullets do
+                if (self._player._bullets[j]) then
+                    if (self._enemies[i]:hit(self._player._bullets[j])) then
+                        table.remove(self._player._bullets, j)
+                        self._enemies[i]._ship._health =
+                            self._enemies[i]._ship._health - BULLETS[self._player._bulletID].damage
+                        if (self._enemies[i]._ship._health <= 0) then
+                            table.remove(self._enemies, i)
+                            break
+                        end
+                    end
                 end
             end
         end
